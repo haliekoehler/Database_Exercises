38aa85a23e5f1e9ee8f0266c30ce5b3bfaa7d365
haliekoehler <koehlerdoom@gmail.com>
2017-01-24 14:34:06 -0600

    Basic Statements exercises - select_exercise.sql file

---
+++ src/select_exercise.sql
@@ -0,0 +1,19 @@
+USE codeup_test_db;
+
+SELECT 'All Pink Floyd Albums' AS 'Info';
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT 'Year Sgt. Pepper''s Lonely Hearts Club Band was released' AS 'Info';
+SELECT release_date FROM albums WHERE name = 'Sgt. Peppers Lonely Hearts Club Band';
+
+SELECT 'Genre of Nirvanas album Nevermind' AS 'Info';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+
+SELECT 'All albums released in the 90s' AS 'Info';
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+
+SELECT 'Albums less than 20 million certified sales' AS 'Info';
+SELECT name FROM albums WHERE sales < 20000000;
+
+SELECT 'All (only) rock albums' AS 'Info';
+SELECT name FROM albums WHERE genre = 'rock';
